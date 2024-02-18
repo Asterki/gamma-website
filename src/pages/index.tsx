ebad23429852a99c4f7a4929d3dea6107328b834
Fernando Rivera <asterki.dev@proton.me>
2024-02-18 08:51:38 -0600

    Feat: small styling change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,33 +13,54 @@ const IndexPage = () => {
                 <title>Stalker G.A.M.M.A.</title>
             </Head>
 
-            <main className="flex items-center justify-center w-full">
-                <section className="flex flex-col items-center justify-center w-full">
-                    <div className="bg-green-400/60 w-full p-4 flex items-center justify-center text-white/50 text-2xl box-shadow-md shadow-green-400/30 shadow-lg">
-                        <FontAwesomeIcon
-                            icon={faDiscord}
-                            className="mx-2 transition-all hover:text-white cursor-pointer"
-                        />
-                        <FontAwesomeIcon
-                            icon={faGithub}
-                            className="mx-2 transition-all hover:text-white cursor-pointer"
-                        />
-                        <FontAwesomeIcon
-                            icon={faDownload}
-                            className="mx-2 transition-all hover:text-white cursor-pointer"
-                        />
+            <main className="flex flex-col items-center justify-center w-full">
+                <section className="flex flex-col items-center justify-center w-full z-10">
+                    <div className="relative">
+                        <div className="max-h-auto lg:min-h-[32rem] overflow-y-hidden overflow-x-hidden absolute ">
+                            <img
+                                src="/images/banner.png"
+                                alt=""
+                                className="opacity-20 md:w-full w-[200%]"
+                            />
+                        </div>
+                        <div className="max-h-auto lg:h-[32rem] w-full flex items-center justify-between flex-col py-10">
+                            <img
+                                src="/images/splash.png"
+                                alt=""
+                                className="max-h-[16rem]"
+                            />
+                            <div className="max-h-56 lg:max-h-[4rem] w-full p-4 flex items-center justify-center text-white/50 text-2xl box-shadow-md">
+                                <FontAwesomeIcon
+                                    icon={faDiscord}
+                                    className="mx-4 transition-all hover:text-white cursor-pointer text-[40px]"
+                                />
+                                <FontAwesomeIcon
+                                    icon={faGithub}
+                                    className="mx-4 transition-all hover:text-white cursor-pointer text-[40px]"
+                                />
+                                <FontAwesomeIcon
+                                    icon={faDownload}
+                                    className="mx-4 transition-all hover:text-white cursor-pointer text-[40px]"
+                                />
+                            </div>
+                            <div className="max-h-56 lg:max-h-[8rem] bg-neutral-700/20 text-white w-full mt-8 py-6 px-10 md:px-56 text-center shadow-lg text-lg backdrop-blur-md">
+                                STALKER GAMMA (gigantic automated modular
+                                modpack for anomaly) is a completely free
+                                stalker mod. it combines over 300 addons made by
+                                dozens of modders as well as myriad of
+                                specifically-developped addons in a complete
+                                overhaul of the stalker anomaly experience
+                            </div>
+                        </div>
                     </div>
-                    <img
-                        src="/images/splash.png"
-                        className="w-5/12 mt-20"
-                        alt=""
-                    />
+                </section>
 
-                    <div className="flex items-center w-full justify-center">
+                <section className="w-full">
+                    <div className="flex md:flex-row flex-col items-center w-full justify-center mt-16">
                         <Link
                             href="https://discord.gg/stalker-gamma"
                             target="_blank"
-                            className="text-center w-3/12 mx-2 p-4 border-2 border-green-400/30 text-white rounded-md text-2xl font-bold bg-green-400/20 transition-all hover:bg-green-400/40"
+                            className="text-center w-11/12 md:w-3/12 my-2 md:mx-2 p-4 border-2 border-green-400/20 text-white rounded-md text-2xl font-bold bg-green-400/20 transition-all hover:bg-green-400/40"
                         >
                             Download G.A.M.M.A.
                             <FontAwesomeIcon
@@ -51,7 +72,7 @@ const IndexPage = () => {
                         <Link
                             href="https://discord.gg/stalker-gamma"
                             target="_blank"
-                            className="text-center w-3/12 mx-2 p-4 border-2 border-blue-400/30 text-white rounded-md text-2xl font-bold bg-blue-400/20 transition-all hover:bg-blue-400/40"
+                            className="text-center w-11/12 md:w-3/12 my-2 md:mx-2 p-4 border-2 border-blue-400/30 text-white rounded-md text-2xl font-bold bg-blue-400/20 transition-all hover:bg-blue-400/40"
                         >
                             Join Discord
                             <FontAwesomeIcon
@@ -61,6 +82,18 @@ const IndexPage = () => {
                         </Link>
                     </div>
                 </section>
+
+                {/* <section className="w-full">
+                    <div className="bg-green-300/20 flex flex-col items-center justify-center">
+                        <div className="absolute">
+                            <img
+                                src="/images/banner.png"
+                                className="w-6/12 opacity-30 blur-md"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </section> */}
             </main>
         </div>
     );
