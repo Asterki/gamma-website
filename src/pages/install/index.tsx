9da2b082fc8f8a238456ddfdbac6a36d83cb071f
Fernando Rivera <asterki.dev@proton.me>
2024-02-28 17:46:15 -0600

    Feat: added min requirements

--- src/pages/install/index.tsx
+++ src/pages/install/index.tsx
@@ -28,6 +28,8 @@ const InstallIndex = () => {
                         RANDOM TITLE
                     </h1>
 
+                    <p>This page is under heavy development and may change.</p>
+
                     <AcordionComponent defaultOpen={true} title="Disclaimer">
                         <p className="p-4">
                             This modpack is 100% copyright free since it
@@ -62,38 +64,34 @@ const InstallIndex = () => {
                         </p>
                     </AcordionComponent>
 
-                    <AcordionComponent defaultOpen={true} title="Requirements">
+                    <AcordionComponent
+                        defaultOpen={true}
+                        title="Minimum Requirements"
+                    >
                         <p className="p-4">
-                            This modpack is 100% copyright free since it
-                            redistributes little to no copyrighted content
-                            developed by other modders. Indeed, G.A.M.M.A.
-                            downloads every add-on directly from moddb or
-                            github, and installs them automatically. You will
-                            thus directly support modders by installing
-                            G.A.M.M.A. On top of this, each addon original
-                            source (moddb or github page) is directly accessible
-                            from the Mod Organizer 2 instance by right-clicking
-                            addons {">"} visit moddb.com.
+                            Stalker GAMMA is a very CPU intensive game, below
+                            are noted the minimum requirements to run Stalker
+                            GAMMA
+                            <p>
+                                <b>Note:</b> Visual and gameplay settings may be
+                                tweaked to better perform in your computer.{" "}
+                                <Link
+                                    className="text-blue-400 hover:underline"
+                                    href="/"
+                                >
+                                    Learn More
+                                </Link>
+                            </p>
                         </p>
 
-                        <br />
-
-                        <p className="p-4">
-                            GAMMA is a large project using the work of many
-                            talented modders, and each and everyone one of them
-                            are contributing to making Stalker Anomaly a better
-                            experience. I would thus like to thank all the
-                            modders involved in this project by making all the
-                            GAMMA addons (indicated by G.A.M.M.A... in MO2) free
-                            to use, edit and repost with modifications as long
-                            as the source is indicated (original addon author,
-                            from Stalker GAMMA). Repost of unmodified individual
-                            parts of this project are not recommended and may
-                            harm the community in the long run because updates
-                            happen quite often in this repository and it is thus
-                            better to directly post links to the relevant files
-                            stored here.
-                        </p>
+                        <ul className="p-4">
+                            <li>OS: Windows 7 or later</li>
+                            <li>Processor: Intel Core i5-2500K</li>
+                            <li>Memory: 8 GB RAM</li>
+                            <li>Graphics: Nvidia GeForce GTX 760</li>
+                            <li>DirectX: DX10 Onwards (DX8 and DX9 may be used by following <Link className="text-blue-400 hover:underline" href="/">these steps after installing Stalker GAMMA</Link>)</li>
+                            <li>Storage: 100 GB available space (SSD Recommended)</li>
+                        </ul>
                     </AcordionComponent>
                 </section>
             </main>
