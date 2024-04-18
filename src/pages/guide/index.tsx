d1bd36927af7947d9700f2096b3d2d6530456712
asterki <asterki.dev@proton.me>
2024-04-18 17:25:28 -0600

    Fix: Fixed top bar on guide page

--- src/pages/guide/index.tsx
+++ src/pages/guide/index.tsx
@@ -4,18 +4,19 @@ import NavbarComponent from "@/components/navbar";
 
 const GuideIndex = () => {
     return (
-        <div className="bg-[#171717] min-h-screen w-full">
+        <div className="bg-[#171717] min-h-screen w-full text-white">
             <Head>
-                <title>Stalker G.A.M.M.A. | Guide</title>
+                <title>Stalker G.A.M.M.A. | Install</title>
             </Head>
 
-            <NavbarComponent page="guide" />
-
-            <section className="flex items-center justify-center w-full">
-                <h1 className="max-h-56 lg:max-h-[8rem] bg-neutral-700/20 text-white w-full mt-8 py-6 px-10 md:px-56 text-center shadow-lg text-lg ">
-                    Gameplay Guide
-                </h1>
-            </section>
+            <main className="flex flex-col items-center justify-center w-full">
+                <NavbarComponent page="guide" />
+                <section className="flex items-center justify-center w-full">
+                    <h1 className="max-h-56 lg:max-h-[8rem] bg-neutral-700/20 text-white w-full mt-8 py-6 px-10 md:px-56 text-center shadow-lg text-lg ">
+                        Gameplay Guide
+                    </h1>
+                </section>
+            </main>
         </div>
     );
 };
